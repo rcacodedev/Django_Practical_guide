7387c30d9e14a087adb53959960de71d0d95f080
rcacodedev <rcacodedv@gmail.com>
2024-01-16 18:39:35 +0100

    Install views

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+
 def index(request):
     return render(request, "blog/index.html")
+
+def posts(request):
+    return render(request, "blog/posts.html")
+
+def post_detail(request):
+    return render(request, "blog/post_detail.html")
