01312fbe04cd37c23fd578df92ea2419c4c5ed7e
rcacodedev <rcacodedv@gmail.com>
2024-01-18 09:30:32 +0100

    final

--- book_store/book_outlet/admin.py
+++ book_store/book_outlet/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'rating', 'author', 'is_bestselling')
+
+
+admin.site.register(Book, BookAdmin)
