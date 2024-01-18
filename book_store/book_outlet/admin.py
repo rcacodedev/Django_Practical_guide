106236a1fdd1029a1734227b72ba055d5e2c87a2
rcacodedev <rcacodedv@gmail.com>
2024-01-18 09:31:45 +0100

    final admin

--- book_store/book_outlet/admin.py
+++ book_store/book_outlet/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from .models import Book
 
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'rating', 'author', 'is_bestselling')
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('title', 'rating', 'author', 'is_bestselling')
+    list_display = ('title', 'author', 'rating', 'is_bestselling')
 
 
 admin.site.register(Book, BookAdmin)
