360fc9278669442d2ebd02c156199be4cd491f17
rcacodedev <rcacodedv@gmail.com>
2024-01-19 09:59:01 +0100

    Final Blog

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -5,23 +5,50 @@ from django.urls import reverse
 from django.utils.text import slugify
 
 # Create your models here.
+# One too One
+class Address(models.Model):
+    street = models.CharField(max_length=80)
+    postal_code = models.CharField(max_length=10)
+    city = models.CharField(max_length=50)
 
+    def __str__(self):
+        return f"{self.street}, {self.postal_code}, {self.city}"
+    
+    class Meta:
+        verbose_name_plural = "Address Entries"
+
+# One to many
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+    address = models.OneToOneField(Address, on_delete=models.CASCADE, null=True)
 
-    def __str__(self):
+    def full_name(self):
         return f"{self.first_name} {self.last_name}"
 
+    def __str__(self):
+        return self.full_name()
+    
+# Many to many
+class Country(models.Model):
+    name = models.CharField(max_length=80)
+    code = models.CharField(max_length=2)
+
+    class Meta:
+        verbose_name_plural = "Countries"
+
+    def __str__(self):
+        return self.name
 
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(5)])
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True, related_name="books")
     is_bestselling = models.BooleanField(default=False)
     # Harry Potter 1 => harry-potter-1
     slug = models.SlugField(default="", blank=True, null=False, db_index=True)
+    published_countries = models.ManyToManyField(Country)
 
     def get_absolute_url(self):
         return reverse("book-detail", args=[self.slug])
--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,73 +1,16 @@
-from django.shortcuts import render
-from datetime import date
+from django.shortcuts import render, get_object_or_404
 
-list_posts = [
-    {
-        "slug": "hike-in-the-mountains",
-        "image": "mountains.jpg",
-        "author": "Max",
-        "date": date(2021, 7, 21),
-        "title": "Mountain Hiking",
-        "excerpt": "I had the best week ever with my family. We went for a week on the mountains.",
-        "content": """
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis nobis
-        aperiam est praesentium, quos.
+from .models import Post
 
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis nobis
-        aperiam est praesentium, quos.
-
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis nobis
-        aperiam est praesentium, quos.
-        """
-    },
-    {
-        "slug": "hike-in-the-mountains",
-        "image": "woods.jpg",
-        "author": "Max",
-        "date": date(2021, 7, 21),
-        "title": "Woods",
-        "excerpt": "I had the best week ever with my family. We went for a week on the mountains.",
-        "content": """
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis nobis
-        aperiam est praesentium, quos.
-
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis nobis
-        aperiam est praesentium, quos.
-
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis nobis
-        aperiam est praesentium, quos.
-        """
-    },
-    {
-        "slug": "hike-in-the-mountains",
-        "image": "coding.jpg",
-        "author": "Max",
-        "date": date(2021, 7, 21),
-        "title": "Programming",
-        "excerpt": "I had the best week ever with my family. We went for a week on the mountains.",
-        "content": """
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis nobis
-        aperiam est praesentium, quos.
-
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis nobis
-        aperiam est praesentium, quos.
-
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis nobis
-        aperiam est praesentium, quos.
-        """
-    },
-]
-
-def get_date(post):
-    return post['date']
 def index(request):
-    sorted_posts = sorted(list_posts, key=get_date)
-    latest_posts = sorted_posts[-3:]
-    return render(request, "blog/index.html", {"posts": latest_posts})
+    posts = Post.objects.all().order_by("-date")[:3]
+    return render(request, "blog/index.html", {"posts": posts})
 
 def posts(request):
+    list_posts = Post.objects.all().order_by("-date")
     return render(request, "blog/posts.html", {"all_posts": list_posts})
 
 def post_detail(request, slug):
-    identified_post = next(post for post in list_posts if post['slug'] == slug)
-    return render(request, "blog/post_detail.html", {"post": identified_post})
+    identified_post = get_object_or_404(Post, slug=slug)
+    return render(request, "blog/post_detail.html", {"post": identified_post,
+                                                     "post_tags": identified_post.tag.all()})
