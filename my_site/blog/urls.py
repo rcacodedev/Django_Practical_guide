ed7e5e4cf98a85fdb6b4c320aaf2c0e363452ce3
rcacodedev <rcacodedv@gmail.com>
2024-01-22 09:59:41 +0100

    update blog

--- my_site/blog/urls.py
+++ my_site/blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("posts/", views.posts, name="posts"),
-    path("posts/<slug:slug>", views.post_detail, name="post_detail"),
+    path("", views.StartingPageView.as_view(), name="index"),
+    path("posts/", views.AllPostsView.as_view(), name="posts"),
+    path("posts/<slug:slug>", views.SinglePostView.as_view(), name="post_detail"),
 ]
--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,16 +1,41 @@
 from django.shortcuts import render, get_object_or_404
-
+from django.views.generic import ListView, DetailView
 from .models import Post
 
-def index(request):
-    posts = Post.objects.all().order_by("-date")[:3]
-    return render(request, "blog/index.html", {"posts": posts})
+# def index(request):
+    # posts = Post.objects.all().order_by("-date")[:3]
+    # return render(request, "blog/index.html", {"posts": posts})
+class StartingPageView(ListView):
+    template_name = "blog/index.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "posts"
 
-def posts(request):
-    list_posts = Post.objects.all().order_by("-date")
-    return render(request, "blog/posts.html", {"all_posts": list_posts})
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        data = queryset[:3]
+        return data
+    
+# def posts(request):
+    # list_posts = Post.objects.all().order_by("-date")
+    # return render(request, "blog/posts.html", {"all_posts": list_posts})
 
-def post_detail(request, slug):
-    identified_post = get_object_or_404(Post, slug=slug)
-    return render(request, "blog/post_detail.html", {"post": identified_post,
-                                                     "post_tags": identified_post.tag.all()})
+class AllPostsView(ListView):
+    template_name = "blog/posts.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "all_posts"
+
+# def post_detail(request, slug):
+    # identified_post = get_object_or_404(Post, slug=slug)
+    # return render(request, "blog/post_detail.html", {"post": identified_post,
+                                                    #  "post_tags": identified_post.tag.all()})
+    
+class SinglePostView(DetailView):
+    template_name = "blog/post_detail.html"
+    model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["post_tags"] = self.object.tag.all()
+        return context
